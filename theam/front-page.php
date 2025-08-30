6691e879b8b13e97b3bfe1abecce53ff03de3e3c
abinadwan <abinadwan@gmail.com>
2025-08-30 23:14:25 +0300

    Create front-page.php

---
+++ theam/front-page.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+
+<section class="homeBanner_home_banner__iw2nI">
+    <video autoplay muted loop playsinline class="homeBanner_banner_video__jcyz4">
+        <source src="https://customer-vdvrwvsen4rlh8t1.cloudflarestream.com/ffa746b2d98b80c7c1af8bb3586b05c1/downloads/default.mp4" type="video/mp4">
+    </video>
+    <div class="homeBanner_banner_content__dyRo6">
+        <h1>توسيع الآفاق، <br><span>في كل مكان</span></h1>
+    </div>
+    <div class="homeBanner_banner_bottom_content__0lafx">
+        <ul>
+            <li><a href="/project/residential">سكني</a></li>
+            <li><a href="/project/office-spaces">مكتبي</a></li>
+            <li><a href="/project/commercial">تجاري</a></li>
+            <li><a href="/project/whiteland">أرض بيضاء</a></li>
+        </ul>
+    </div>
+</section>
+
+<section class="projects_section">
+    <div class="container">
+        <h2 class="main_title">المشاريع</h2>
+        <div class="swiper projects_project_slider__86o9t">
+            <div class="swiper-wrapper">
+                <!-- سيتم ملؤها ديناميكيًا -->
+            </div>
+            <div class="swiper-pagination"></div>
+            <div class="custom-button-prev"></div>
+            <div class="custom-button-next"></div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
