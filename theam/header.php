8167db23e9caad6cdb7e51ebe4bf535910641a95
abinadwan <abinadwan@gmail.com>
2025-08-30 23:14:42 +0300

    Create header.php

---
+++ theam/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> dir="rtl">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header class="header">
+    <div class="container">
+        <a href="/" class="site-logo">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-new.svg" alt="شعار الشركة الجديد" width="89" height="169">
+        </a>
+        <ul class="header_nav_right_links__E5QpC">
+            <li><a href="mailto:info@newcompany.com">info@newcompany.com</a></li>
+            <li><a href="tel:+966500000000">+966 50 000 0000</a></li>
+        </ul>
+        <div class="header_social_icons_wrap__uwXCW">
+            <h5>تابعنا</h5>
+            <ul class="social_icons">
+                <li><a href="https://linkedin.com/company/newcompany"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/in.png" alt="LinkedIn"></a></li>
+                <li><a href="https://x.com/newcompany"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tweet.png" alt="Twitter"></a></li>
+                <li><a href="https://instagram.com/newcompany"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ins.png" alt="Instagram"></a></li>
+            </ul>
+        </div>
+        <div class="header_lang_wrap__L3xAA"><a href="/en">EN</a></div>
+    </div>
+</header>
